2c76a2b4122fe184dda65cf3532f5b1a549d0e01
pawaters <86101754+pawaters@users.noreply.github.com>
2022-01-20 17:08:09 +0200

    solver compiles but does not work. divide testing and find solution

--- TestFiles/main_pw.c
+++ TestFiles/main_pw.c
@@ -14,9 +14,25 @@
 
 int	main (void)
 {
-	t_map	*map;
-	int	map_size = 5;
+
+// Test: new_map, print_map, map_size 5	
 	
-	map = new_map(map_size);
-	print_map(map, map_size);
+//	t_map	*map;
+//	int	map_size = 5;
+//	
+//	map = new_map(map_size);
+//	print_map(map, map_size);
+
+// Test: create a sample piece list and print elements of struct
+
+	t_piece first = {{0, 0, 0, 1, 1, 0, 1, 1}, 'A', 0, 0, NULL};
+	t_piece second = {{0, 0, 0, 1, 0, 2, 0, 3}, 'B', 0, 0, NULL};
+	t_piece third = {{0, 0, 0, 1, 0, 2, 0, 3}, 'C', 0, 0, NULL};
+	t_piece fourth = {{0, 0, 0, 1, 0, 2, 0, 3}, 'D', 0, 0, NULL};
+	first.next = &second;
+	second.next = &third;
+	third.next = &fourth;
+	t_piece *head = &first;
+
+	solve(head);
 }
