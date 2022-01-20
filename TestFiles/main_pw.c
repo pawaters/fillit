7558a64c72b76da6ec86d968138ea5ba16cf7373
pawaters <86101754+pawaters@users.noreply.github.com>
2022-01-20 13:46:31 +0200

    structure of tests added

---
+++ TestFiles/main_pw.c
@@ -0,0 +1,15 @@
+#include "../fillit.h"
+
+/* TEST FILE FOR SOLVER.C & MAP.C - STRUCTURE
+ * Compenent tests:
+ * - create a sample piece_list, with 3 pieces;
+ * - print that piece_list coordinates, and tetrimino.
+ * - print a sample 5*5 empty map,
+ *
+ * Solver tests:
+ * - with 3 pieces that only need horizontal moves
+ * - with 4 pieces that requires map growth
+ * - with 4 pieces, requires 1st piece shifted with backtracking
+*/
+
+
