1c166ad9194f2b45f68e32549981c8c956862b59
Martin Silen <martin@silen.io>
2022-01-24 11:18:00 +0200

    norm ok

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: msilen <msilen@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 07:33:20 by msilen            #+#    #+#             */
-/*   Updated: 2022/01/24 07:38:47 by msilen           ###   ########.fr       */
+/*   Updated: 2022/01/24 11:16:59 by msilen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,14 @@
 int	main(int argc, char **argv)
 {
 	t_piece	*piecelist;
-	
+
 	if (argc != 2)
 	{
 		ft_putstr("usage: ./fillit source_file\n");
 		return (1);
 	}
-	if ((piecelist = parser(argv[1])) == NULL)
+	piecelist = parser(argv[1]);
+	if (piecelist == NULL)
 	{
 		ft_putstr("error\n");
 		return (1);
